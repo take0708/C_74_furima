cca97b932b456b62017d30b37f4f30486ca64dd6
keisuke.f <f.keisuke0526@gmail.com>
2020-07-29 16:09:49 +0900

    add column to shipping_infos table

--- db/migrate/20200729064926_create_shipping_infos.rb
+++ db/migrate/20200729064926_create_shipping_infos.rb
@@ -1,17 +1,17 @@
 class CreateShippingInfos < ActiveRecord::Migration[5.2]
   def change
     create_table :shipping_infos do |t|
-      t.string :family_name, null: false
-      t.string :first_name, null: false
+      t.string :family_name,      null: false
+      t.string :first_name,       null: false
       t.string :family_name_kana, null: false
-      t.string :first_name_kana, null: false
-      t.integer :postcode, null: false
-      t.integer :prefecture, null: false
-      t.string :city, null: false
-      t.string :house_number, null:false
-      t.string :building, null: false
-      t.integer :phone_number, 
-
+      t.string :first_name_kana,  null: false
+      t.integer :postcode,        null: false
+      t.integer :prefecture,      null: false
+      t.string :city,             null: false
+      t.string :house_number,     null: false
+      t.string :building,         null: false
+      t.integer :home_call_num, 
+      t.references :user_id,      null: false, foreign_key: true
       t.timestamps
     end
   end
