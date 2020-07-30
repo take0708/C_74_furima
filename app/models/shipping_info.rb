aadf955545229815c28001e1c9a1ce2829e33193
keisuke.f <f.keisuke0526@gmail.com>
2020-07-30 11:18:05 +0900

    add validates

--- app/models/shipping_info.rb
+++ app/models/shipping_info.rb
@@ -1,2 +1,7 @@
 class ShippingInfo < ApplicationRecord
+  belongs_to :user
+
+  validates :family_name, :first_name, :postcode, :prefecture, :city, :house_number, :user_id, presence: true
+  # ↓正規表現でカタカナだけのバリデーションをかける(複雑そうなので後回し)
+  validates :family_name_kana, :first_name_kana, presence: true
 end
