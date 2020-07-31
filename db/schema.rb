30418b79e8c1396e55c277ae7d7a64ac97551ffb
anderson-da-silva <kenji.hirabayasi@gmail.com>
2020-08-01 04:11:25 +0900

    make top page 7

--- db/schema.rb
+++ db/schema.rb
@@ -38,15 +38,6 @@ ActiveRecord::Schema.define(version: 2020_07_29_064926) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.integer "birth_year", null: false
-    t.integer "birth_month", null: false
-    t.integer "birth_day", null: false
-    t.text "introduction"
-    t.string "icon"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
