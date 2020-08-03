ded843e70d1c3df2da2808d3342a71dde596d029
keisuke.f <f.keisuke0526@gmail.com>
2020-08-03 19:04:28 +0900

    add optional: true

--- app/models/shipping_info.rb
+++ app/models/shipping_info.rb
@@ -1,7 +1,7 @@
 class ShippingInfo < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
-  validates :family_name, :first_name, :postcode, :prefecture, :city, :house_number, :user_id, presence: true
+  validates :family_name, :first_name, :postcode, :prefecture, :city, :house_number, presence: true
   # ↓正規表現でカタカナだけのバリデーションをかける(複雑そうなので後回し)
   validates :family_name_kana, :first_name_kana,  presence: true
 end
