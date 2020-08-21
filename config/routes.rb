6309fd0f62ee38cc88f299715b8c5348b312eb22
keisuke.f <f.keisuke0526@gmail.com>
2020-08-21 23:18:46 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     get 'users/new_address', to: 'users/registrations#new_address'
     post 'users/create_address', to: 'users/registrations#create_address'
   end
+
+  resources :users, only: [:show]
+
   root 'items#index'
   resources :categories, only: [:index, :show] 
   resources :creditcards, only: [:index, :new, :create, :show, :destroy] do
