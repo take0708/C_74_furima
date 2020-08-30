28210f40f8a6dc0be4c9471e6437293fe053e9f4
keisuke.f <f.keisuke0526@gmail.com>
2020-08-30 12:55:01 +0900

    add user_id for items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_085553) do
+ActiveRecord::Schema.define(version: 2020_08_30_035019) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_085553) do
     t.integer "shippingday_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
