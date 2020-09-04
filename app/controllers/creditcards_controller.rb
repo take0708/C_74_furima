18597690f866d764aa90ce2467787d592e124e87
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2020-09-04 21:45:21 +0900

    gets_items_list_top_view

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -4,12 +4,13 @@ class CreditcardsController < ApplicationController
   before_action :set_item, only:[:show, :pay]
   before_action :card_present,only:[:index,:destroy]
   before_action :take_card, only:[:show, :pay]
-  before_action :set_customer, only:[:index, :destroy]
-  before_action :set_card_information, only:[:index]
+  before_action :set_customer, only:[:destroy]
   before_action :set_api_key
 
   def index
     if @card
+      set_customer
+      set_card_information
       @card_brand = @card_information.brand
       #画像を保存して使用した方がいいのか？
       case @card_brand
