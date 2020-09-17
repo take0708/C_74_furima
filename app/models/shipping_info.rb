745d030e08f758110f4461fabf3e904770a0eb3b
keisuke.f <f.keisuke0526@gmail.com>
2020-09-17 17:34:05 +0900

    edit model

--- app/models/shipping_info.rb
+++ app/models/shipping_info.rb
@@ -1,6 +1,6 @@
 class ShippingInfo < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :family_name, :first_name, :postcode, :prefecture, :city, :house_number, presence: true
-  validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }, allow_blank: true # カタカナのみ
+  validates :family_name, :first_name, :postcode, :prefecture, :city, :house_number, :family_name_kana, :first_name_kana, presence: true
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }, allow_blank: true # カタカナのみ
 end
