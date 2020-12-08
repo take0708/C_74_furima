b6da933727959f3c41f929c36daff65379a5bc3d
take0708 <pk_er_stbo@yahoo.co.jp>
2020-12-08 20:26:37 +0900

    ElasticIP変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,1 @@
-server '54.150.152.169', user: 'ec2-user', roles: %w{app db web}
+server '54.248.184.142', user: 'ec2-user', roles: %w{app db web}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_044551) do
+ActiveRecord::Schema.define(version: 2020_09_18_042043) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -72,11 +72,6 @@ ActiveRecord::Schema.define(version: 2020_09_18_044551) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_shipping_infos_on_user_id"
-  end
-
-  create_table "sign_ups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
